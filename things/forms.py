311c6b521be67f53647474e6849bc94890a3e090
Rulaa Alhazzani <q.rxr@outlook.sa>
2023-12-07 20:59:03 +0000

    defined a form and displayed it in the home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,13 @@
-"""Forms of the project."""
+# things/forms.py
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator  # Add this import
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        exclude = ['created_at']
+
+    description = forms.CharField(widget=forms.Textarea)
+    quantity = forms.IntegerField(widget=forms.NumberInput, validators=[MinValueValidator(0), MaxValueValidator(50)])
--- things/views.py
+++ things/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, redirect
+from .forms import ThingForm
+from .models import Thing
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            # Process the valid form data, e.g., save to the database
+            form.save()
+            return redirect('home')  # Redirect to the home page or any other page
+    else:
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
